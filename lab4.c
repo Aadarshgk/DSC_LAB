12c8ab068635a47e06e844591b8fef8f938288fa
Aadarshgk <aadarshgk04@gmail.com>
2023-12-13 22:16:42 +0530

    Lab3 program

--- lab4.c
+++ lab4.c
@@ -2,9 +2,19 @@
 #include<ctype.h>
 
 int s[20], top=-1;
+
+void push(){
+}
+
+int isEmpty(){
+    return top == -1;
+}
  
 void main(){
     char a[20], x;
+    int i=0;
     printf("Enter the expression to be converted to postfix notation:");
     scanf("%s",a);
+    while(a[i]!='\0'){
+    }
 }
