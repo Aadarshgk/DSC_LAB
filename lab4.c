ef821784c65236b6376a3de89ab0919644b708b0
Aadarshgk <aadarshgk04@gmail.com>
2023-12-13 22:10:49 +0530

    Added lab4 program

---
+++ lab4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int s[20], top=-1;
+ 
+void main(){
+    char a[20], x;
+    printf("Enter the expression to be converted to postfix notation:");
+    scanf("%s",a);
+}
