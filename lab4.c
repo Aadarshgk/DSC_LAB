f443124df429f9824eeba70acc1d3227a5a264b2
Aadarshgk <aadarshgk04@gmail.com>
2023-12-14 23:37:55 +0530

    Lab4 program

--- lab4.c
+++ lab4.c
@@ -1,62 +1,91 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 
 char s[20];
-int top=-1;
+int top = -1;
 
-void push(char a){
-    s[++top];
+void push(char a)
+{
+    s[++top]=a;
 }
 
-char pop(){
-    if(top==-1){
+char pop()
+{
+    if (top == -1)
+    {
         return -1;
     }
-
-    return s[top--];
-}
-
-int priority(char a){
-    if(a=='(' || a==')'){
-        return 0;
-    }else if(a=='+' || a=='-'){
-        return 1;
-    }else if(a=='*' || a=='/'){
-        return 2;
+    else
+    {
+        return s[top--];
     }
 }
 
-int isEmpty(){
+int priority(char a)
+{
+    if (a == '(' || a == ')')
+    {
+        return 0;
+    }
+    else if (a == '+' || a == '-')
+    {
+        return 1;
+    }
+    else if (a == '*' || a == '/')
+    {
+        return 2;
+    }
+return 0;
+}
+
+int isEmpty()
+{
     return top == -1;
 }
- 
-void main(){
-    char a[20], x;
-    int i=0;
-    printf("Enter the expression to be converted to postfix notation:");
-    scanf("%s",a);
-    while(a[i]!='\0'){
 
-        if(a[i]=='('){
+void main()
+{
+    char a[20], x;
+    int i = 0;
+    printf("Enter the expression to be converted to postfix notation:");
+    scanf("%s", a);
+
+    printf("The postfix expression is : ");
+
+    while (a[i] != '\0')
+    {
+
+        if (a[i] == '(')
+        {
             push(a[i]);
         }
-        
-        else if(isalnum(a[i])){
-            printf("%c",a[i]);
+
+        else if (isalnum(a[i]))
+        {
+            printf("%c", a[i]);
         }
 
-        else if(a[i]==')'){
-            while((x=pop())!='('){
-                printf("%c",pop());
+        else if (a[i] == ')')
+        {
+            while ((x = pop()) != '(')
+            {
+                printf("%c", x);
             }
         }
 
-        else{
-            if(!isEmpty() && priority(a[i])>=priority(a[i])){
-                printf("%c",pop());
+        else
+        {
+            while (!isEmpty() && priority(s[top]) >= priority(a[i]))
+            {
+                printf("%c", pop());
             }
             push(a[i]);
         }
         i++;
+    }
+
+    while (!isEmpty())
+    {
+        printf("%c", pop());
     }
 }
