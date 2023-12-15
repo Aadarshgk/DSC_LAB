11d5a8c99940f04d1cc8cb40a314a14b848ed628
Aadarshgk <aadarshgk04@gmail.com>
2023-12-15 23:34:45 +0530

    Lab 4b program

--- lab4b.c
+++ lab4b.c
@@ -1,6 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
+
+int s[20],top = -1;
+
+void push(int a){
+    return s[++top]=a;
+}
+
+int pop(){
+    return s[top--];
+}
+
+
 
 void main(){
+    char a[20];
+    int num;
+    int i=0;
+    printf("Enter the postfix notation to evaluate output:");
+    scanf("%s",a);
+    while(a[i]!='\0'){
+        if(isdigit(a[i])){
+            num= a[i] - '0';
+            push(num);
+        }
+        else{
 
+
+        }
+
+    i++;
+    }
 }
