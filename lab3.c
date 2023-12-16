39ab870bf57c48a31c887488a0c887874a1efce7
Aadarshgk <aadarshgk04@gmail.com>
2023-12-16 23:08:48 +0530

    Added lab3 program

--- lab3.c
+++ lab3.c
@@ -1,3 +1,15 @@
+/*STACK of Integers (Array Implementation of Stack with maximum
+size MAX)
+a) Push an Element on to Stack
+b) Pop an Element from Stack
+c) Demonstrate how Stack can be used to check Palindrome
+d) Demonstrate Overflow and Underflow situations on Stack
+e) Display the status of Stack
+f) Exit
+Support the program with appropriate functions for each of the above
+operations */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 5
