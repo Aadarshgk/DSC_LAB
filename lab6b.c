f80feb36c87ca2f3c4b0b8531aaa193400bbdd10
Aadarshgk <aadarshgk04@gmail.com>
2024-01-09 15:30:12 +0530

    Added lab6 program

---
+++ lab6b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char A, char B, char C)
+{
+    if (n == 1)
+    {
+        printf("Move ring from %c to %c ", A, C);
+    }
+    else{
+        towerOfHanoi(n-1,A,C,B);
+        towerOfHanoi(1,A,B,C);
+        towerOfHanoi(n-1,B,A,C);
+    }
+}
+
+void main()
+{
+    int n;
+    printf("Enter the number of rings:");
+    scanf("%d",&n);
+    towerOfHanoi(n,'A','B','C');
+}
