1ea9c4103d0b7fc385562106e6abd9b9255d6121
Aadarshgk <aadarshgk04@gmail.com>
2024-01-29 20:57:42 +0530

    Added insert at middle and insert after functions

--- lab9.c
+++ lab9.c
@@ -21,9 +21,28 @@ void insert_at_start(int num){
 }
 
 void insert_at_end(int num){
-
+    node* nn= getnode();
+    nn->data=num;
+    node* temp=start;
+    while(temp->next!=NULL){
+        temp= temp->next;
+    }
+    temp->next= nn;
 }
 
+void insert_after(int key, int num){
+    node* nn= getnode();
+    nn->data= num;
+    node* temp = start;
+    while(temp->data!= key){
+        temp= temp->next;
+        if(temp->next== NULL){
+            printf("Key not found.\n")
+        }
+    }
+    nn->next= temp->next;
+    temp->next= nn;
+}
 
 void delete();
 void display();
