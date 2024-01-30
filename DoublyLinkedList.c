6ec3222f2fc980644442becb1deedb19e072fba8
Aadarshgk <aadarshgk04@gmail.com>
2024-01-30 16:43:45 +0530

    Added insert functions

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -9,11 +9,53 @@ typedef struct node_st{
 
 node* start= NULL;
 
-void insert_at_end();
-void insert_at_start();
-void insert_after();
+node*getnode(){
+    node* nn= (node* )malloc(sizeof(node));
+    nn->next=nn->prev= NULL;
+}
 
-void delete_at_end();
+void insert_at_start(int num){
+    node* nn= getnode();
+    nn->data=num;
+    if(start==NULL){
+        start= nn;
+    }else{
+        nn->next= start;
+        start->prev= nn;
+        start=nn;
+        return;
+    }
+}
+
+void insert_at_end(int num){
+    node* nn= getnode();
+    nn->data=num;
+    node* temp=start;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next= nn;
+    nn->prev= temp;
+}
+
+void insert_after(int key,int num){
+    node* temp=start;
+    while(temp->next!=NULL){
+        if(temp->data== key){
+            node* nn= getnode();
+            nn->data= num;
+            nn->prev=temp;
+            nn->next=temp->next;
+            temp->next= nn;
+            return;
+        }
+        temp= temp->next;
+    }
+    printf("Key not found.\n");
+    return;
+}
+
+void delete_at_start();
 void delete_at_end();
 void delete_after();
 
