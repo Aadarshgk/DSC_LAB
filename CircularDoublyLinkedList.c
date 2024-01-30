92e2dfb3c7adaf93cd79e59cafb26cae1f92d7aa
Aadarshgk <aadarshgk04@gmail.com>
2024-01-30 20:07:17 +0530

    Added insertion functions

--- CircularDoublyLinkedList.c
+++ CircularDoublyLinkedList.c
@@ -1,8 +1,50 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void insert_at_start();
-void insert_at_start();
+typedef struct node_st{
+    int data;
+    struct node_st* next;
+    struct node_st* prev;
+}node;
+
+node* start= NULL;
+
+node*getnode(){
+    node* nn= (node* )malloc(sizeof(node));
+    nn->next=nn->prev= NULL;
+}
+
+void insert_at_start(int num){
+    node* nn= getnode();
+    nn->data=num;
+    if(start==NULL){
+        start= nn;
+        nn->next=start;
+        nn->prev=start;
+        return;
+    }
+    
+    nn->prev= start->prev;
+    nn->next=start;
+    start->prev->next=nn;
+    start->prev=nn;
+    start=nn;
+}
+void insert_at_end(int num){
+    node* nn= getnode();
+    nn->data= num;
+    if(start==NULL){
+        start= nn;
+        nn->next=start;
+        nn->prev=start;
+        return;
+    }
+
+    nn->prev=start->prev;
+    start->prev->next= nn;
+    nn->next=start;
+    start->prev=nn;        
+}
 
 void delete_at_start();
 void delete_at_end();
