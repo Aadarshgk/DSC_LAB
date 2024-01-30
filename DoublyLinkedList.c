84f71579030cee6c296e352198dd77334678b364
Aadarshgk <aadarshgk04@gmail.com>
2024-01-30 17:08:02 +0530

    Added delete function

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -55,9 +55,50 @@ void insert_after(int key,int num){
     return;
 }
 
-void delete_at_start();
-void delete_at_end();
-void delete_after();
+void delete_at_start(){
+    if(start==NULL){
+        printf("List is empty.");
+        return;
+    }
+    node* toDelete= start;
+    start= start->next;
+    free(toDelete);
+    start->prev=NULL;
+}
+void delete_at_end(){
+    if(start==NULL){
+        printf("List is empty.");
+        return;
+    }
+    node* temp= start;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->prev->next= NULL;
+    free(temp);
+}
+void delete_after(int Key){
+    node* temp=start;
+    while(temp->next!=NULL){
+        if(temp->data==Key){
+            if(temp==start){
+                delete_at_start();
+                return;
+            }else if(temp->next==NULL){
+                delete_at_end();
+                return;
+            }else{
+                temp->prev->next=temp->next;
+                temp->next->prev=temp->prev;
+                free(temp);
+                return;
+            }
+        }
+        temp=temp->next;
+    }
+    printf("Key not found.");
+    return;
+}
 
 void display();
 
