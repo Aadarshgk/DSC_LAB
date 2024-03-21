8809b7f761f56e731e8a0c976f462452b891f851
Aadarshgk <aadarshgk04@gmail.com>
2024-03-21 11:47:34 +0530

    Added lab 11 program

--- lab11.c
+++ lab11.c
@@ -1,3 +1,10 @@
+// Write a C program to support the following operations on a doubly
+// linked list.
+// a) Insert a new node to the left of the node whose key value is read as
+// an input.
+// b) Delete a node with given data, if it is found otherwise display
+// appropriate error message. 
+
 #include <stdio.h>
 #include <stdlib.h>
 
